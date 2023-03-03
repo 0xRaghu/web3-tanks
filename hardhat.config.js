f83a4acfae9c46ce597ecc81c19657f2d33b6a03
Raghu Thiyagharajan <shriraghuraman@gmail.com>
2023-03-02 20:22:11 -0500

    config setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,34 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("solidity-coverage");
+const dotenv = require("dotenv");
+require("hardhat-docgen");
+dotenv.config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    gasPrice: 21,
+    url: `http://127.0.0.1:8545`,
+  },
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
+  docgen: {
+    path: "./docs",
+    clear: true,
+    runOnCompile: true,
+  },
 };
